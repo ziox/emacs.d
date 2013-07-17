fb882242274b5b388195a394c21aba03175989d6
Nicola Pasta <nicola.pasta@gmail.com>
2013-07-17 10:38:25 +0200

    Going interactive with ido-mode and smex

--- init.el
+++ init.el
@@ -11,6 +11,15 @@
 (el-get 'sync)
 
 (setq my:el-get-packages
-      '(powerline))
+      '(powerline
+	smex))
 
 (el-get 'sync my:el-get-packages)
+
+;; Interactive Mode
+(ido-mode t)
+
+;; Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
