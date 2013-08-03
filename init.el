a241a1cfa24dbdbe08a5934f23a388a11e1de7fa
Nicola Pasta <nicola.pasta@gmail.com>
2013-08-03 22:50:48 +0200

    C++ initial configuration

--- init.el
+++ init.el
@@ -72,6 +72,35 @@
 ;; I want <RET> to auto indent! WTF!
 (global-set-key (kbd "RET") 'newline-and-indent)
 
+;; C/C++
+(defconst ziox-style
+  '((c-tab-always-indent        . t)
+    (c-comment-only-line-offset . 4)
+    (c-hanging-braces-alist     . ((substatement-open after)
+                                   (brace-list-open)))
+    (c-hanging-colons-alist     . ((member-init-intro before)
+                                   (inher-intro)
+                                   (case-label after)
+                                   (label after)
+                                   (access-label after)))
+    (c-cleanup-list             . (scope-operator
+                                   empty-defun-braces
+                                   defun-close-semi))
+    (c-offsets-alist            . ((arglist-close . c-lineup-arglist)
+                                   (substatement-open . 0)
+                                   (case-label        . 4)
+                                   (block-open        . 0)
+                                   (knr-argdecl-intro . -)
+                                   (comment-intro     . 0)))
+    (c-echo-syntactic-information-p . t))
+  "Personal C Programming Style")
+(c-add-style "ziox" ziox-style)
+
+(setq c-default-style
+      '((java-mode . "java")
+        (awk-mode . "awk")
+        (other . "ziox")))
+
 ;; Smex
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
