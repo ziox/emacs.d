bf13521f3dd4a8e0de41ef5a8b2b106eeffcc211
Nicola Pasta <nicola.pasta@gmail.com>
2014-05-28 21:31:13 +0200

    Activate CUA mode (GUI only)

--- init-gui.el
+++ init-gui.el
@@ -7,6 +7,12 @@
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 4) ((control) . nil)))
 (setq mouse-wheel-progressive-speed nil)
 
+;; CUA mode for fuck sake!
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil)
+(transient-mark-mode 1)
+(setq cua-keep-region-after-copy t)
+
 ;; on Mac, there's always a menu bar drown, don't have it empty
 (if (eq system-type 'darwin)
   (menu-bar-mode t))
