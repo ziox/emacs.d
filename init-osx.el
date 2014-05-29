269335fad9d8c3c67e7391d96df2cd4ccb2157e5
Nicola Pasta <nicola.pasta@gmail.com>
2014-05-29 22:15:00 +0200

    Add some ergo key-bindings

--- init-osx.el
+++ init-osx.el
@@ -24,3 +24,13 @@
 
 (global-set-key (kbd "s-<right>") 'move-end-of-line)
 (global-set-key (kbd "s-<left>") 'back-to-indentation)
+
+(define-minor-mode ziox-keys-osx-mode
+  "Personal key-bindings for OS X"
+  :lighter " ZX"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "s-i") 'previous-line)
+            (define-key map (kbd "s-k") 'next-line)
+            (define-key map (kbd "s-j") 'left-char)
+            (define-key map (kbd "s-l") 'right-char)
+            map))
